d36f89240afd668d468eeb7d7807bca47b92935a
leongarci <leongarcia1669@gmail.com>
2024-10-09 14:01:45 +0200

    fichier etape 2

---
+++ src/controllers/HomeController.ts
@@ -0,0 +1,17 @@
+import { createFactory } from 'hono/factory'
+import { logger } from 'hono/logger'
+
+// ...
+
+const factory = createFactory()
+
+const middleware = factory.createMiddleware(async (c, next) => {
+  c.set('foo', 'bar')
+  await next()
+})
+
+const handlers = factory.createHandlers(logger(), middleware, (c) => {
+  return c.json(c.var.foo)
+})
+
+app.get('/api', ...handlers)
